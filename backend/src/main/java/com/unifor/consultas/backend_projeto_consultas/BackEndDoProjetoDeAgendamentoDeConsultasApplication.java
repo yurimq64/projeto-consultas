ecc5f34846269074525e89d719e2cde063668ffe
yuri <ymendesqueiroz@gmail.com>
2025-10-28 19:56:18 -0300

    Initial commit

---
+++ backend/src/main/java/com/unifor/consultas/backend_projeto_consultas/BackEndDoProjetoDeAgendamentoDeConsultasApplication.java
@@ -0,0 +1,13 @@
+package com.unifor.consultas.backend_projeto_consultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndDoProjetoDeAgendamentoDeConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndDoProjetoDeAgendamentoDeConsultasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/unifor/consultas/backend_projeto_consultas/BackEndDoProjetoDeAgendamentoDeConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifor.consultas.backend_projeto_consultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndDoProjetoDeAgendamentoDeConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
