825d2dcf5f7369773a910aa9a0366de644774a25
yuri <ymendesqueiroz@gmail.com>
2025-11-27 18:43:32 -0300

    feat: Configura estrutura do AuthModule com JWT e Bcrypt

--- backend/api-agendamento/src/app.module.ts
+++ backend/api-agendamento/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PacientesModule } from './pacientes/pacientes.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PacientesModule, PrismaModule],
+  imports: [PacientesModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/api-agendamento/src/pacientes/pacientes.service.ts
+++ backend/api-agendamento/src/pacientes/pacientes.service.ts
@@ -1,38 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePacienteDto } from './dto/create-paciente.dto';
 import { UpdatePacienteDto } from './dto/update-paciente.dto';
-import { PrismaService } from '../prisma/prisma.service'; // 👈 Importamos nossa ponte com o banco
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PacientesService {
   
-  // 1. Injeção de Dependência
-  // Pedimos ao NestJS para nos dar o PrismaService pronto para uso
   constructor(private prisma: PrismaService) {}
 
-  // 2. Criar (Create)
   async create(data: CreatePacienteDto) {
     return this.prisma.paciente.create({
-      data, // O DTO já tem os campos certos (nome, telefone, usuarioId)
+      data,
     });
   }
 
-  // 3. Listar Todos (Read All)
   async findAll() {
     return this.prisma.paciente.findMany({
-      include: { usuario: true }, // Traz também os dados de Login (email) junto
+      include: { usuario: true },
     });
   }
 
-  // 4. Buscar por ID (Read One)
   async findOne(id: number) {
     return this.prisma.paciente.findUnique({
       where: { id },
-      include: { consultas: true }, // Traz o histórico de consultas desse paciente
+      include: { consultas: true },
     });
   }
 
-  // 5. Atualizar (Update)
   async update(id: number, data: UpdatePacienteDto) {
     return this.prisma.paciente.update({
       where: { id },
@@ -40,7 +34,6 @@ export class PacientesService {
     });
   }
 
-  // 6. Deletar (Delete)
   async remove(id: number) {
     return this.prisma.paciente.delete({
       where: { id },
